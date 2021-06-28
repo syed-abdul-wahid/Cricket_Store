87900ea5e680b7d211a5e49ac2d7d346824cd50e
Syed Wahid <syedabdulwahid21.cs@gmail.com>
2021-06-28 16:42:09 +0530

    Created Basic Category Grid

---
+++ src/Components/Categories/Categories.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Categories.css";
+const Categories = () => {
+  return (
+    <div className="Category__Container">
+      <div className="Category__Batting">Batting</div>
+      <div className="Category__Bowling">Bowling</div>
+      <div className="Category__WicketKeeping">WicketKeeping</div>
+      <div className="Category__Accessories">Accessories</div>
+    </div>
+  );
+};
+
+export default Categories;
